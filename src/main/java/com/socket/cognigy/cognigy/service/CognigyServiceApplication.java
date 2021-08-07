4c27a595b0cff82515749674218b3e34fc9313dd
saindhav <sain.nihal@gmail.com>
2021-08-07 12:39:19 +0530

    Initial commit

---
+++ src/main/java/com/socket/cognigy/cognigy/service/CognigyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.socket.cognigy.cognigy.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognigyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognigyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socket/cognigy/cognigy/service/CognigyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socket.cognigy.cognigy.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CognigyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
